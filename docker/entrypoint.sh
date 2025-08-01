0bb90f6f42025ea20b099abf2760300e66e86d1c
Alex Kuhrt <alex@qrt.de>
2025-08-01 23:25:47 +0200

    Hello world

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,11 @@
 
 if [ -d /tmp/gamedir ]
 then
+  echo Found additional files, syncing...
   rsync --chown=steam:steam /tmp/gamedir/* /opt/steam/hldm/valve
 fi
 
 export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
 
-echo "Starting HLDS Docker ${VERSION} (${RELEASE_DATE})..."
+echo "Starting HLDS Docker ${VERSION} (${RELEASE_DATE}/${COMMIT_ID})..."
 ./hlds_linux "${@}"
